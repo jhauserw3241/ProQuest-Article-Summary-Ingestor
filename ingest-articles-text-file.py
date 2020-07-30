73d4ae061702244e650c4ca15c593b57ea0ba338
Joy Hauser <jhauser@ksu.edu>
2020-07-29 19:03:40 -0500

    Parse information from text file

--- ingest-articles-text-file.py
+++ ingest-articles-text-file.py
@@ -11,12 +11,17 @@ ARTICLE_SEPARATOR = "___________________________________________________________
 
 def main():
     # Get the start time of the current process
-    print("Start")
     start = datetime.now()
 
     # Open the Excel file
     workbook = xlsxwriter.Workbook(OUTPUT_XLSX_FILE)
     worksheet = workbook.add_worksheet()
+
+    # Storage objects
+    article = {}
+    headers = ["Title", "Abstract", "ISSN", "Document type", "Author", "Publication info", "Links", "Full text", "Credit", "Subject", "Location", "Publication title", "Publication year", "Publication date", "Section", "Publisher", "Place of publication", "Country of publication", "Publication subject", "Source type", "Language of publication", "First page" "ProQuest document ID", "Document URL", "Copyright", "Last updated", "Database"]
+    article_info_line_index = 0
+    current_header = ""
 
     # Open the input text file
     with open(INPUT_FILE, "r") as input_file:
@@ -27,18 +32,69 @@ def main():
                 continue
             # Handle line separators 
             elif line == ARTICLE_SEPARATOR:
-                continue
+                for key in article:
+                    print(key + ": " + article[key])
+                
+
+
+                # Empty out the article object
+                article = {}
+                current_header = ""
+                article_info_line_index = 0
+            # Handle rows with content
             else:
-                print(line)
+                # Remove the newline from the end of the line
+                line_text = line.split("\n")[0]
+
+                # Attempt to pull the header out of the line
+                line_parts = line_text.split(": ")
+                line_header = ""
+                line_contents = ""
+                # Handle lines where there is a value that looks like a header
+                if len(line_parts) > 1:
+                    line_header = line_parts[0]
+                    line_contents = ": ".join(line_parts[1:len(line_parts)])
+                # Handle lines where there does not appear to be a header
+                elif len(line_parts) == 1:
+                    line_contents = line_parts[0]
+
+                # Handle where there is an unexpected header
+                if line_header != "" and line_header in headers:
+                    # Update the current header for the content
+                    current_header = line_header
+                else:
+                    # Add the value that was assumed to be a header back to the content
+                    line_contents = ": ".join(line_parts[0:len(line_parts)])
+                
+                # Add a key with the current header value to the article representation if it doesn't exist
+                if line_header not in article:
+                    article[current_header] = ""
+
+                # Handle the short hand for the article title
+                if article_info_line_index == 0:
+                    continue
+                # Handle the relevant links for the artcle
+                elif current_header == "Links":
+                    if article[current_header] == "":
+                        article[current_header] = line_contents
+                    else:
+                        article[current_header] = "{}\n{}".format(article[current_header], line_contents)
+                # Separate out the document URL from the publication information
+                elif current_header == "Publication info" and article_info_line_index == 2:
+                    article["Document URL"] = line_contents
+                # Handle every other piece of information
+                else:
+                    article[current_header] = "{}\n\n{}".format(article[current_header], line_contents)
+
+            article_info_line_index += 1
 
     # Close the Excel file
     workbook.close()
 
     # Print out the total time taken to run the script
-    print("End")
     end = datetime.now()
-    diff_time = start - end
-    print("Total Time: {} seconds".format(diff_time.seconds / 3600))
+    diff_time = end - start
+    print("Total Time: {} seconds".format(diff_time.total_seconds()))
 
 if __name__ == "__main__":
     main()
